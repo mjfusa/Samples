5df51c04ae2d076aa26c4539ebe9c85bbb1b7e53
Mike Francis <mike.francis@live.com>
2019-03-20 14:26:45 -0700

    List all running processes, including UWP and thier suspended state.

---
+++ EnumerateProcesses/Program.cs
@@ -0,0 +1,32 @@
+﻿/**
+List all running processes, including UWP and thier suspended state.
+ * **/
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Process[] processlist = Process.GetProcesses();
+
+            foreach (Process theprocess in processlist)
+            {
+
+                bool bSuspended = false;
+                if (theprocess.Threads.Count > 0)
+                    bSuspended =  (theprocess.Threads[0].ThreadState == ThreadState.Wait) ? (theprocess.Threads[0].WaitReason == ThreadWaitReason.Suspended) : false;
+   
+                Console.WriteLine("Process: {0} ID: {1} Suspended: {2}", theprocess.ProcessName, theprocess.Id, bSuspended);
+            }
+        }
+    }
+}
