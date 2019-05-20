2b18b40d5b3f0c92dca06e5d462057f9b3982514
Mike Francis <mike.francis@live.com>
2019-05-20 13:47:41 -0700

    Update StartHere

--- electron/getstarted/starthere.cmd
+++ electron/getstarted/starthere.cmd
@@ -1,3 +1,4 @@
+cd %USERPROFILE%
 PowerShell Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/mjfusa/Samples/master/electron/getstarted/setpath.ps1' -OutFile 'setpath.ps1'
 PowerShell Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/mjfusa/Samples/master/electron/getstarted/installtools.cmd' -OutFile 'installtools.cmd'
 rem 1st run install choco
