a2f1fb3be03a1f7467154292a28a84db49b5e19a
Mike Francis <mike.francis@live.com>
2019-05-22 16:19:47 -0700

    update startjere.cmd

--- electron/getstarted/starthere.cmd
+++ electron/getstarted/starthere.cmd
@@ -1,4 +1,3 @@
 cd %USERPROFILE%
-PowerShell Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/mjfusa/Samples/master/electron/getstarted/setpath.ps1' -OutFile 'setpath.ps1'
 PowerShell Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/mjfusa/Samples/master/electron/getstarted/installtools.cmd' -OutFile 'installtools.cmd'
 cmd.exe /c installtools.cmd
