80d80560fcba7e5beebf107f20a486eb70954b6c
Mike Francis <mikefrancis@appconsulti.onmicrosoft.com>
2019-06-10 16:36:31 -0700

    update installtools.cmd

--- electron/getstarted/installtools.cmd
+++ electron/getstarted/installtools.cmd
@@ -13,9 +13,7 @@ powershell Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('
 set "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin;c:\program files\nodejs;C:\Program Files\Git\cmd"
 for %%a in (git.install, nodejs, 7zip.install, googlechrome, netfx-4.6.1-devpack) do choco install %%a -y
 cmd /c npm install --global --production windows-build-tools@4.0.0 -y
-choco install windows-sdk-10.1 --version 10.1.17134.12 -y
-copy "c:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Tools\MSVC\14.16.27023\lib\x86\store\references\platform.winmd" "C:\Windows\Microsoft.NET\Framework64\v4.0.30319\"   
-copy "C:\Program Files (x86)\Windows Kits\10\UnionMetadata\10.0.17134.0\windows.winmd" "C:\Program Files (x86)\Windows Kits\10\UnionMetadata"
+choco install windows-sdk-10-version-1809-all -y
 
 :eds
 md \EDS
@@ -27,6 +25,8 @@ cmd /c npm config set msvs_version 2015 --global
 cmd /c npm config set python %userprofile%\.windows-build-tools\python27\python.exe
 cmd /c PowerShell [Environment]::SetEnvironmentVariable('VCTargetsPath', 'C:\Program Files (x86)\MSBuild\Microsoft.Cpp\v4.0\V140', 'User')
 SET VCTargetsPath=C:\Program Files (x86)\MSBuild\Microsoft.Cpp\v4.0\V140
+copy "c:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Tools\MSVC\14.16.27023\lib\x86\store\references\platform.winmd" "C:\Windows\Microsoft.NET\Framework64\v4.0.30319\"   
+copy "C:\Program Files (x86)\Windows Kits\10\UnionMetadata\10.0.17763.0\windows.winmd" "C:\Program Files (x86)\Windows Kits\10\UnionMetadata"
 cmd.exe /c npm install
 cmd.exe /c npm install electron-builder
 cmd.exe /c .\node_modules\\.bin\electron-builder -w
